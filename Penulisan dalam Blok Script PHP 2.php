eeec141b1727460eb4ff6983d787fa0c0aad86ec
Fatma20 <fatma.azza05@gmail.com>
2017-05-29 13:14:04 +0700

    Add files via upload

---
+++ Penulisan dalam Blok Script PHP 2.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Contoh Pemakaian Teks Komentar</title>
+</head>
+<body>
+<?php
+/* echo "Aku sekarang bisa PHP! <br>";
+print "Mudah dan menyenangkan <br>";
+?>
+<?php echo ("inijuga bisa <br>"); */ ?>
+
+<script language="php">
+echo ("tulis pake ini jika html editor Kita tidak mengenali PHP");
+/* teks komentar satu baris */
+
+</script>
+</body>
+</html>
